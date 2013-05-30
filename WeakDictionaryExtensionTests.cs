6bf606c9b1f806fd1b97ed97e08e05fdfa897452
Brian Dukes <bdukes@engagesoftware.com>
2013-05-30 10:43:42 -0500

    Add Brandon Haynes' implementation with my tests

---
+++ WeakDictionaryExtensionTests.cs
@@ -0,0 +1,175 @@
+﻿namespace ExtensionCandidates
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.UI;
+    using System.Xml.Linq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CollectionExtensionTests : AssertionHelper
+    {
+        [Test]
+        public void get_null_string_from_hashtable_for_missing_value()
+        {
+            var table = new Hashtable { { "app id", "abc123" } };
+
+            var value = table.Get<string>("cat id");
+
+            Expect(value, Is.Null);
+        }
+
+        [Test]
+        public void can_get_string_from_hashtable()
+        {
+            var table = new Hashtable { { "app id", "abc123" } };
+
+            var value = table.Get<string>("app id");
+
+            Expect(value, Is.EqualTo("abc123"));
+        }
+
+        [Test]
+        public void get_string_from_hashtable_when_default_is_provided()
+        {
+            var table = new Hashtable { { "app id", "abc123" } };
+
+            var value = table.GetOrDefault("app id", "abracadabra");
+
+            Expect(value, Is.EqualTo("abc123"));
+        }
+
+        [Test]
+        public void can_get_default_string_from_hashtable()
+        {
+            var table = new Hashtable { { "app id", "abc123" } };
+
+            var value = table.GetOrDefault("cat id", "Frank");
+
+            Expect(value, Is.EqualTo("Frank"));
+        }
+
+        [Test]
+        public void can_get_bool_from_hashtable()
+        {
+            var table = new Hashtable { { "app id", "true" } };
+
+            var value = table.Get<bool>("app id");
+
+            Expect(value, Is.True);
+        }
+
+        [Test]
+        public void get_bool_from_hashtable_when_default_is_provided()
+        {
+            var table = new Hashtable { { "app id", "true" } };
+
+            var value = table.GetOrDefault("app id", false);
+
+            Expect(value, Is.True);
+        }
+
+        [Test]
+        public void can_get_default_bool_from_hashtable()
+        {
+            var value = true;
+            var table = new Hashtable { { "app id", "abc123" } };
+
+            value = table.GetOrDefault("Allow Windows Live Writer", value);
+
+            Expect(value, Is.True);
+        }
+
+        [Test]
+        public void get_false_from_hashtable_for_missing_value()
+        {
+            var table = new Hashtable { { "app id", "abc123" } };
+
+            var value = table.Get<bool>("Allow Windows Live Writer");
+
+            Expect(value, Is.False);
+        }
+
+        [Test]
+        public void get_bool_with_custom_converter_from_hashtable()
+        {
+            var table = new Hashtable { { "allow", "on" } };
+
+            var value = table.Get(
+                "allow",
+                v =>
+                {
+                    bool allowed;
+                    if (!bool.TryParse(v, out allowed))
+                    {
+                        allowed = v.Equals("on", StringComparison.Ordinal);
+                    }
+
+                    return allowed;
+                });
+
+            Expect(value, Is.True);
+        }
+
+        [Test]
+        public void get_int_from_statebag()
+        {
+            var collection = new StateBag { { "appId", "123" } };
+
+            var value = collection.Get<int>("appId");
+
+            Expect(value, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void get_datetime_from_xmlnode()
+        {
+            var sampleDateTime = new DateTime(2013, 5, 30, 5, 6, 7);
+            var node = new XElement(
+                "parent",
+                new XElement("startDate", sampleDateTime));
+            var nodeDictionary = node.Descendants().ToDictionary(element => element.Name.ToString(), element => element.Value);
+
+            var value = nodeDictionary.Get<DateTime>("startDate");
+
+            Expect(value, Is.EqualTo(sampleDateTime));
+        }
+
+        [Test]
+        public void can_get_timespan_with_custom_converter()
+        {
+            var collection = new Hashtable { { "length", "1:10:10" } };
+
+            var value = collection.Get("length", TimeSpan.Parse);
+
+            Expect(value, Is.EqualTo(TimeSpan.FromSeconds(4210)));
+        }
+
+        [Test]
+        public void throws_argumentnullexception_when_dictionary_is_null()
+        {
+            IDictionary dictionary = null;
+
+            Expect(() => dictionary.Get<int>("value ID"), Throws.TypeOf<ArgumentNullException>().With.Property("ParamName").EqualTo("dictionary"));
+        }
+
+        [Test]
+        public void throws_invalidcastexception_when_type_is_not_supported()
+        {
+            var dictionary = new Dictionary<string, string> { { "length", "1:10:10" } };
+
+            Expect(() => dictionary.Get<TimeSpan>("length"), Throws.TypeOf<InvalidCastException>());
+        }
+
+        [Test]
+        public void throws_formatexception_when_value_cannot_be_parsed()
+        {
+            var dictionary = new Dictionary<string, string> { { "ID", "abc123" } };
+
+            Expect(() => dictionary.Get<int>("ID"), Throws.TypeOf<FormatException>());
+        }
+    }
+}
---
+++ WeakDictionaryExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace ExtensionCandidates
+{
+    public static class WeakDictionaryExtensions
+    {
+        public static T Get<T>(this IDictionary dictionary, string name, Func<object, T> converter = null)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+            else if (!dictionary.Contains(name))
+                throw new ArgumentException("name");
+            else
+                return (converter ?? DefaultConversion<T>)(dictionary[name]);
+        }
+
+        public static T Get<T>(this IDictionary dictionary, string name, Func<string, T> converter)
+        { return dictionary.Get(name, (object value) => converter(value != null ? value.ToString() : null)); }
+
+        public static T GetOrDefault<T>(this IDictionary dictionary, string name, T defaultValue = default(T), Func<object, T> converter = null)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+            else
+                return dictionary.Contains(name)
+                           ? (converter ?? DefaultConversion<T>)(dictionary[name])
+                           : defaultValue;
+        }
+
+        public static T GetOrDefault<T>(this IDictionary dictionary, string name, T defaultValue, Func<string, T> converter)
+        { return dictionary.GetOrDefault(name, defaultValue, (object value) => converter(value != null ? value.ToString() : null)); }
+
+        private static T DefaultConversion<T>(object value)
+        {
+            if (value is IConvertible)
+                return (T)Convert.ChangeType(value, typeof(T));
+            else
+                return value is T ? (T)value : default(T);
+        }
+    }
+
+    public static class NameValueCollectionExtensions
+    {
+        public static ILookup<string, string> ToLookup(this NameValueCollection collection)
+        {
+            return collection.AllKeys
+                             .SelectMany(key => ParseValues(key, collection.GetValues(key)))
+                             .ToLookup(pair => pair.Key, pair => pair.Value);
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> ParseValues(string key, string[] values)
+        {
+            return (values.Length == 1
+                       ? values[0].Split(',')
+                       : values).Select(value => new KeyValuePair<string, string>(key, value));
+        }
+    }
+
+    public static class Usage
+    {
+        private static readonly IDictionary Map = new Hashtable
+            {
+            {"convertable-bool", "true"},
+            {"parsable-bool", "true"},
+            {"onoff-bool", "on"}
+            };
+
+        public static bool ExplicitConversion()
+        { return Map.GetOrDefault("parsable-bool", false, bool.Parse); }
+
+        public static bool ImplicitConversion()
+        { return Map.GetOrDefault<bool>("convertable-bool"); }
+
+        public static bool InlineConversion()
+        { return Map.GetOrDefault("onoff-bool", converter: value => value.Equals("on")); }
+    }
+}
